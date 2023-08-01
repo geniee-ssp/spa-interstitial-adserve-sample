88b04491cddb788268ff1ba99a05d894b5858432
reiya-hattori <reiya.hattori@geniee.co.jp>
2023-08-01 19:22:51 +0900

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, Inject, Renderer2 } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  constructor(private router: Router) {}
+export class AppComponent {
+  private iframeId = 'iframeContent';
+  constructor(
+    private router: Router,
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
+
+  @HostListener('window: beforeunload')
+  ngOnDestroy() {
+    this.removeIframe();
+  }
+
+  private removeIframe() {
+    const iframe = this.document.getElementById(
+      this.iframeId
+    ) as HTMLIFrameElement;
+    if (iframe) {
+      this.renderer.removeChild(this.document.body, iframe);
+    }
+  }
 
   ngOnInit() {
     this.router.events.subscribe((event) => {
@@ -17,13 +37,31 @@ export class AppComponent implements OnInit {
     });
   }
   private executeAdScript() {
+    this.removeIframe();
+
+    const iframe = this.renderer.createElement('iframe') as HTMLIFrameElement;
+    iframe.id = this.iframeId;
+    iframe.scrolling = 'no';
+    Object.assign(iframe.style, {
+      width: '100%',
+      height: '100%',
+      position: 'fixed',
+      top: '0',
+      left: '0',
+      border: '0',
+      zIndex: '2147483647',
+      display: 'none',
+    });
+
+    this.renderer.appendChild(this.document.body, iframe);
+
     ((window, document) => {
       const createIframe = (insteTag: string) => {
         const html = document.documentElement;
         const body = document.body;
         const iframe = document.createElement('iframe');
         const style =
-          'width: 100%;height: 100%;position: fixed;top: 0;left: 0;border: 0;z-index: 1000000000000;';
+          'width: 100%;height: 100%;position: fixed;top: 0;left: 0;border: 0;z-index: 2147483647;';
 
         html.style.overflow = 'hidden';
         body.style.overflow = 'hidden';
