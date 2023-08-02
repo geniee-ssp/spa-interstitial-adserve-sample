f669a0aa7ef894702d7414d7541c450278b31ce8
reiya-hattori <reiya.hattori@geniee.co.jp>
2023-08-02 12:58:41 +0900

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,70 +1,30 @@
-import { Component, Inject, Renderer2 } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { DOCUMENT } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  constructor(
-    private router: Router,
-    private renderer: Renderer2,
-    @Inject(DOCUMENT) private document: Document
-  ) {}
+export class AppComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  ngOnInit() {
-    this.router.events.subscribe((event) => {
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
-        this.executeAdScript();
+        this.handlePageTransition();
       }
     });
-  }
-  private executeAdScript() {
-    ((window, document) => {
-      const createIframe = (insteTag: string) => {
-        const html = document.documentElement;
-        const body = document.body;
-        const iframe = document.createElement('iframe');
-        const style =
-          'width: 100%;height: 100%;position: fixed;top: 0;left: 0;border: 0;z-index: 2147483647;';
 
-        html.style.overflow = 'hidden';
-        body.style.overflow = 'hidden';
-        body.prepend(iframe);
-        iframe.setAttribute('id', 'iframeContent');
-        iframe.scrolling = 'no';
-        iframe.setAttribute('style', style);
-        const iframeContent = document.getElementById(
-          'iframeContent'
-        ) as HTMLIFrameElement;
-        if (!iframeContent || !iframeContent.contentWindow) return;
-        const iframeContentDoc = iframeContent.contentWindow.document;
-        iframeContentDoc.open();
-        iframeContentDoc.write(
-          '<html><head></head><body style="margin:0;padding:0;">' +
-            insteTag +
-            '</body></html>'
-        );
-        iframeContentDoc.close();
-        iframeContentDoc.addEventListener('click', (event: MouseEvent) => {
-          const targetElement = event.target as HTMLElement;
-          if (
-            !targetElement ||
-            targetElement.id !== 'gn_interstitial_close_icon'
-          )
-            return;
-          html.style.overflow = '';
-          body.style.overflow = '';
-          iframe.style.display = 'none';
-          iframe.remove();
-        });
-      };
-      createIframe(
-        '<script type="text/javascript" src="https://js.gsspcln.jp/t/466/310/a1466310.js"></sc' +
-          'ript>'
-      );
-    })(window, document);
+    this.handlePageTransition();
+  }
+
+  handlePageTransition(): void {
+    document.documentElement.classList.add('gn_inst_scroll_cancel');
+
+    const interstitialArea = document.getElementById('gn_interstitial_area');
+    if (interstitialArea) {
+      interstitialArea.style.display = 'block';
+    }
   }
 }
