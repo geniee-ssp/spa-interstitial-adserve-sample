f61a1ae7bb9d1154e6dc5e5e43d07da00e751ae5
reiya-hattori <reiya.hattori@geniee.co.jp>
2023-08-02 11:41:36 +0900

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, Inject, Renderer2 } from '@angular/core';
+import { Component, Inject, Renderer2 } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { DOCUMENT } from '@angular/common';
 
@@ -8,9 +8,6 @@ import { DOCUMENT } from '@angular/common';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  private iframeId = 'iframeContent';
-  private iframe: HTMLIFrameElement | null = null;
-
   constructor(
     private router: Router,
     private renderer: Renderer2,
@@ -25,27 +22,6 @@ export class AppComponent {
     });
   }
   private executeAdScript() {
-    if (this.iframe) {
-      this.iframe.remove();
-      this.iframe = null;
-    }
-
-    // 新しい iframe を作成
-    this.iframe = this.renderer.createElement('iframe') as HTMLIFrameElement;
-    this.iframe.id = this.iframeId;
-    this.iframe.scrolling = 'no';
-    Object.assign(this.iframe.style, {
-      width: '100%',
-      height: '100%',
-      position: 'fixed',
-      top: '0',
-      left: '0',
-      border: '0',
-      zIndex: '2147483647',
-      display: 'none',
-    });
-
-    this.renderer.appendChild(this.document.body, this.iframe);
     ((window, document) => {
       const createIframe = (insteTag: string) => {
         const html = document.documentElement;
