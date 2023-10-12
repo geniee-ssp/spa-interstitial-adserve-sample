032cc9e78f2d03594308f4900edadd123b91500d
reiya-hattori <reiya.hattori@geniee.co.jp>
2023-10-12 10:03:37 +0900

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,53 +27,86 @@ export class AppComponent {
   }
 
   ngOnInit() {
-    console.log("ngOnInit: コンポーネントの初期化時に実行される・一度のみ実行")
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
-        this.serveInterstitialAdService.executeAdScript(event.url);
+        // this.serveInterstitialAdService.executeAdScript(event.url);
       }
     });
-    window.gnshbrequest = window.gnshbrequest || {cmd:[]};
-    window.gnshbrequest.cmd.push(function(){
-      window.gnshbrequest.applyPassback("1550723", "[data-cptid='1550723']");
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        console.log("ngOnInit: コンポーネントの初期化時に実行される・一度のみ実行")
+        window.gnshbrequest = window.gnshbrequest || {cmd:[]};
+        window.gnshbrequest.cmd.push(function(){
+          window.gnshbrequest.applyPassback("1550723", "[data-cptid='1550723']");
+        });
+      }
     });
   }
 
   ngDoCheck() {
-    console.log("ngDoCheck: コンポーネントの状態が変わったことを検知したら実行される")
-    window.gnshbrequest = window.gnshbrequest || {cmd:[]};
-    window.gnshbrequest.cmd.push(function(){
-      window.gnshbrequest.registerPassback("1550723");
-      window.gnshbrequest.rerun();
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        console.log("ngDoCheck: コンポーネントの状態が変わったことを検知したら実行される")
+        // window.gnshbrequest = window.gnshbrequest || {cmd:[]};
+        // window.gnshbrequest.cmd.push(function(){
+        //   window.gnshbrequest.registerPassback("1550723");
+        //   window.gnshbrequest.rerun();
+        // });
+      }
     });
   }
 
   ngAfterContentInit() {
-    console.log("ngAfterContentInit: 外部コンテンツを初期化したときに実行される・一度のみ実行")
-
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+      console.log("ngAfterContentInit: 外部コンテンツを初期化したときに実行される・一度のみ実行")
+      // window.gnshbrequest = window.gnshbrequest || {cmd:[]};
+      //   window.gnshbrequest.cmd.push(function(){
+      //     window.gnshbrequest.registerPassback("1550723");
+      //     window.gnshbrequest.rerun();
+      //   });
+      }
+    });
 
   }
 
   ngAfterContentChecked() {
-    console.log("ngAfterContentChecked: 外部コンテンツの変更を検知したときに実行される")
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
-          window.gnshbrequest = window.gnshbrequest || {cmd:[]};
-          window.gnshbrequest.cmd.push(function(){
-            window.gnshbrequest.applyPassback("1550723", "[data-cptid='1550723']");
-          });
+        console.log("ngAfterContentChecked: 外部コンテンツの変更を検知したときに実行される")  
+          // window.gnshbrequest = window.gnshbrequest || {cmd:[]};
+          // window.gnshbrequest.cmd.push(function(){
+          //   window.gnshbrequest.applyPassback("1550723", "[data-cptid='1550723']");
+          // });
       }
     })
   }
 
   ngAfterViewInit() {
-    console.log("ngAfterViewInit: 自分自身と子コンポーネントのビューの初期化時に実行される・一度のみ実行")
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        console.log("ngAfterViewInit: 自分自身と子コンポーネントのビューの初期化時に実行される・一度のみ実行")
+      window.gnshbrequest = window.gnshbrequest || {cmd:[]};
+        window.gnshbrequest.cmd.push(function(){
+          window.gnshbrequest.registerPassback("1550723");
+          window.gnshbrequest.rerun();
+        });
+      }
+    });
 
   }
 
   ngAfterViewChecked() {
-    console.log("ngAfterViewChecked: 自分自身と子コンポーネントのビューが変更されたときに実行される")
-    console.log("=============================================================================")
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        console.log("ngAfterViewChecked: 自分自身と子コンポーネントのビューが変更されたときに実行される")
+        console.log("=============================================================================")
+        window.gnshbrequest = window.gnshbrequest || {cmd:[]};
+        window.gnshbrequest.cmd.push(function(){
+          window.gnshbrequest.applyPassback("1550723", "[data-cptid='1550723']");
+        });
+      }
+    })
 
   }
 
